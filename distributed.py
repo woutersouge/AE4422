839bbf92e21212cf6bcac3d142980b178f0b0c61
Joseph Krueger <jakrueger98@comcast.net>
2022-11-18 14:15:56 +0100

    Fixed some infinite loops

--- distributed.py
+++ distributed.py
@@ -7,6 +7,7 @@ Code in this file is just provided as guidance, you are free to deviate from it.
 import time as timer
 from single_agent_planner import compute_heuristics, a_star, get_sum_of_cost
 from aircraft import AircraftDistributed
+
 from cbs import detect_collision, detect_collisions
 
 
@@ -48,7 +49,7 @@ class DistributedPlanningSolver(object):
 
         for i in range(self.num_of_agents):
             heur = compute_heuristics(self.my_map, self.goals[i])
-            newAgent = AircraftDistributed(self.my_map, self.starts[i], self.goals[i], heur, i)
+            newAgent = AircraftDistributed(self.my_map, self.starts[i], self.goals[i], heur, i, False)
             agentlist.append(newAgent)
             moves[i].append(self.starts[i])
 
@@ -56,9 +57,10 @@ class DistributedPlanningSolver(object):
             ten = [{} for _ in range(self.num_of_agents)]
             curr = [{} for _ in range(self.num_of_agents)]
             for j in range(self.num_of_agents):
-                print(j)
+                #print(j)
                 ten[j]['loc'], ten[j]['val'], curr[j]['loc'] = agentlist[j].calc_next(curr, look_steps)
-
+            places = [d['loc'] for d in curr]
+            print_mapf_instance(self.my_map, places, self.goals)
             ten_loc = [d['loc'] for d in ten]
 
             coll = False
@@ -122,3 +124,39 @@ class DistributedPlanningSolver(object):
         print(result)
 
         return result, self.CPU_time  # Hint: this should be the final result of the distributed planning (visualization is done after planning)
+
+
+def print_mapf_instance(my_map, starts, goals):
+    """
+    Prints start location and goal location of all agents, using @ for an obstacle, . for a open cell, and
+    a number for the start location of each agent.
+
+    Example:
+        @ @ @ @ @ @ @
+        @ 0 1 . . . @
+        @ @ @ . @ @ @
+        @ @ @ @ @ @ @
+    """
+    print('Start locations')
+    print_locations(my_map, starts)
+
+
+
+def print_locations(my_map, locations):
+    """
+    See docstring print_mapf_instance function above.
+    """
+    starts_map = [[-1 for _ in range(len(my_map[0]))] for _ in range(len(my_map))]
+    for i in range(len(locations)):
+        starts_map[locations[i][0]][locations[i][1]] = i
+    to_print = ''
+    for x in range(len(my_map)):
+        for y in range(len(my_map[0])):
+            if starts_map[x][y] >= 0:
+                to_print += str(starts_map[x][y]) + ' '
+            elif my_map[x][y]:
+                to_print += '@ '
+            else:
+                to_print += '. '
+        to_print += '\n'
+    print(to_print)
